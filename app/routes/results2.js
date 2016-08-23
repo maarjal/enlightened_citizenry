3ee33b5af0fc845e0c19e9c1cbe248f03b4ac2a9
Maarja Laineste and Shradha Pulla <maarja+shradha@>
2016-08-23 09:15:24 -0700

    Add per_page=all to end of url to show all committees

--- app/routes/results2.js
+++ app/routes/results2.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var url = "http://congress.api.sunlightfoundation.com/committees/?apikey=b88f167fdac34860bf4220a9d104ec29";
+    var url = "http://congress.api.sunlightfoundation.com/committees/?apikey=b88f167fdac34860bf4220a9d104ec29&per_page=all";
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
