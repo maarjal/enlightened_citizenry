5cd892483c6aae2e899c45afbe5da8f85f65a62e
Maarja Laineste and Shradha Pulla <maarja+shradha@>
2016-08-23 10:41:36 -0700

    Install ember-truth-helpers to create if statements to sort committees on results2.hbs

--- app/routes/results2.js
+++ app/routes/results2.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend ({
   model: function() {
     var url = "http://congress.api.sunlightfoundation.com/committees/?apikey=b88f167fdac34860bf4220a9d104ec29&per_page=all";
     return Ember.$.getJSON(url).then(function(responseJSON) {
